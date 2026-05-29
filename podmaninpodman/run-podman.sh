0e891c4a4f763951f840672f5a3692070133b277
Alice Frosi <afrosi@redhat.com>
2026-05-29 11:34:28 +0200

    Add missing devices

--- podmaninpodman/run-podman.sh
+++ podmaninpodman/run-podman.sh
@@ -9,6 +9,8 @@ podman run -td --rm --name podman-remote-cont \
 	-w /workspace \
 	--device /dev/fuse \
 	--device /dev/kvm \
+	--device /dev/vhost-vsock \
+	--device /dev/net/tun \
 	-v container-var:/var/lib/containers \
 	-v podman-var:/var/podman \
 	-v podman-socket:/run/podman \
